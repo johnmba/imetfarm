0dd6b0570287b80096117ceb231142381c7f641a
= <=>
2022-07-21 18:44:40 +0100

    allowed debug

--- imetfarm/settings.py
+++ imetfarm/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%(b@g36x%kfp+ks-7nzo==sjjfzp!m$uvmb^q)y2t!x+teu6%v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'imetfarm.com', '.herokuapp.com']
 
