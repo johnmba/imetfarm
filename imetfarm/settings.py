135af4ea71f024878d93b244631e0784679a750c
= <=>
2022-07-21 17:38:16 +0100

    static files on

--- imetfarm/settings.py
+++ imetfarm/settings.py
@@ -211,5 +211,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 import django_heroku
-django_heroku.settings(locals(),staticfiles=False)
+django_heroku.settings(locals())
 #django_heroku.settings(locals())
